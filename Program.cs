d964acbfddcaffb8b0556e08e745f6ccf5071807
LeeonardoFS <148078823+LeeonardoFS@users.noreply.github.com>
2023-12-14 11:43:26 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
-﻿
+    ﻿// Definição da classe Produto que representa um item em estoque
+
 class Produto
 {
+    // Propriedades do produto, como nome, categoria, quantidade e preço
     public string Nome { get; set; }
     public string Categoria { get; set; }
     public int Quantidade { get; set; }
@@ -10,19 +12,21 @@ class Produto
 
 class Program
 {
+    // Dicionário para armazenar produtos no estoque
     static Dictionary<string, Produto> estoque = new Dictionary<string, Produto>();
 
     static void Main()
     {
         while (true)
         {
+            // Menu de opções
             Console.WriteLine("1 - Adicionar Produto");
             Console.WriteLine("2 - Remover Produto");
             Console.WriteLine("3 - Consultar Estoque");
-            Console.WriteLine("4 - Sair");
-
+            Console.WriteLine("4 - Sair");    
             Console.WriteLine();
-
+            
+            // Solicitação da opção ao usuário
             Console.Write("Escolha uma opção: ");
             string opcao = Console.ReadLine();
 
@@ -48,7 +52,7 @@ class Program
         }
     }
     
-
+    // Função para adicionar um novo produto ao estoque
     static void AdicionarProduto()
     {Console.Clear();
         Console.Write("Digite o código do produto: ");
@@ -56,11 +60,13 @@ class Program
 
         if (estoque.ContainsKey(codigo))
         {
+            // Produto já existe no estoque
             Console.WriteLine("Produto já existente. Utilize a opção de alterar quantidade.");
             return;
         }
 
         Produto novoProduto = new Produto();
+         // Solicita informações do novo produto
 
         Console.Write("Digite o nome do produto: ");
         novoProduto.Nome = Console.ReadLine();
@@ -73,7 +79,8 @@ class Program
 
         Console.Write("Digite o preço do produto: ");
         novoProduto.Preço = double.Parse(Console.ReadLine());
-
+     
+        // Adiciona o novo produto ao estoque
         estoque.Add(codigo, novoProduto);
 
         Console.WriteLine();
@@ -92,6 +99,7 @@ class Program
 
         if (estoque.ContainsKey(codigo))
         {
+            // Produto encontrado no estoque
             Console.Write($"Remover {estoque[codigo].Nome} ({estoque[codigo].Categoria}) ({estoque[codigo].Preço}) - Quantidade atual: {estoque[codigo].Quantidade}. Digite a quantidade a ser removida: ");
             int quantidade = int.Parse(Console.ReadLine());
 
@@ -99,6 +107,7 @@ class Program
 
             if (quantidade <= estoque[codigo].Quantidade)
             {
+                // Remove a quantidade especificada do estoque
                 estoque[codigo].Quantidade -= quantidade;
                 Console.WriteLine($"Produto {codigo} removido com sucesso! Quantidade restante: {estoque[codigo].Quantidade}");
             
@@ -107,6 +116,7 @@ class Program
             }
             else
             {
+                // Quantidade insuficiente em estoque
                 Console.WriteLine("Quantidade insuficiente em estoque.");
             }
         
@@ -115,6 +125,7 @@ class Program
         }
         else
         {
+            // Produto não encontrado no estoque
             Console.WriteLine("Produto não encontrado no estoque.");
         }
     
@@ -126,7 +137,8 @@ class Program
     static void ConsultarEstoque()
     {Console.Clear();
         Console.WriteLine("Estoque Atual:");
-
+     
+    // Exibe informações de todos os produtos no estoque
         foreach (var produto in estoque)
         {
             Console.WriteLine($"Código: {produto.Key}, Nome: {produto.Value.Nome}, Categoria: {produto.Value.Categoria}, Preço: {produto.Value.Preço}, Quantidade: {produto.Value.Quantidade}   unidades");
